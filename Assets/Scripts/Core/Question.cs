983919fc50c7e00e31f4eecba9c85d178ab7b64d
Maddie <129887899+MaddieeeP@users.noreply.github.com>
2024-09-28 12:02:55 +0100

    Base Unity project

---
+++ Assets/Scripts/Core/Question.cs
@@ -0,0 +1,14 @@
+public class Question
+{
+    private string _qText;
+    private string _aText;
+
+    public string qText { get { return _qText; } }
+    public string aText { get { return _aText; } }
+
+    public Question(string questionText, string answerText)
+    {
+        _qText = questionText;
+        _aText = answerText;
+    }
+}
