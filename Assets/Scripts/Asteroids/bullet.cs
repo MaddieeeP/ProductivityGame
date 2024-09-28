91cf844a74ee40038d0aeb6ce893e2a84b427105
CWSODA <76740111+CWSODA@users.noreply.github.com>
2024-09-28 16:09:07 +0100

    a lot of stuff

--- Assets/Scripts/Asteroids/bullet.cs
+++ Assets/Scripts/Asteroids/bullet.cs
@@ -6,12 +6,6 @@ public class bullet : MonoBehaviour
 {
     public float bullet_speed = 20f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
